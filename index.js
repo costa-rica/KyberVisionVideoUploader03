a57e84c40fd6255cf8223d5bcc6032b4b9cd8f52
Nick <nrodrig1@gmail.com>
2025-04-05 12:06:30 +0200

    added error logging

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ const path = require("path");
 const { checkAudio } = require("./modules/checkAudio");
 const { processVideo } = require("./modules/processVideo");
 const { saveVideo } = require("./modules/helpers");
+const fs = require("fs");
 
 const fileName = process.argv[2]; // The video file name passed as an argument
+const filePath = path.join(process.env.PATH_TO_UPLOADED_VIDEOS, fileName);
+const directoryPath = path.dirname(filePath);
 
 if (!fileName) {
-  console.error(
+  throw new Error(
     "❌ No file name provided. Please pass a file name as an argument."
   );
-  process.exit(1);
 }
 
-const filePath = path.join(process.env.PATH_TO_UPLOADED_VIDEOS, fileName);
+if (!fs.existsSync(directoryPath)) {
+  const errorMessage = `❌ Directory not found: ${directoryPath}`;
+  console.error(errorMessage);
+  throw new Error(errorMessage);
+} else {
+  console.log(`✅ Directory exists: ${directoryPath}`);
+}
 
 (async () => {
   try {
--- modules/checkAudio.js
+++ modules/checkAudio.js
@@ -3,13 +3,17 @@ const ffmpeg = require("fluent-ffmpeg");
 const checkAudio = (filePath) => {
   return new Promise((resolve, reject) => {
     ffmpeg.ffprobe(filePath, (err, metadata) => {
-      if (err) return reject(err);
+      if (err) {
+        console.error("Error with ffprobe", err.message);
+        return reject(err);
+      }
 
       const hasAudioStream = metadata.streams.some(
         (stream) => stream.codec_type === "audio"
       );
       // ✅ Log progress before resolving the promise
       console.log("percent complete: 10");
+      console.log(`Audio stream found: ${hasAudioStream}`);
       resolve(hasAudioStream);
     });
   });
